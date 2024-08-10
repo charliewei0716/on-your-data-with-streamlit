c4170dc4d79dd56d55f26311812b66fea757ca88
Charlie Wei <91643249+charliewei0716@users.noreply.github.com>
2024-08-10 07:57:47 +0000

    Update

--- app.py
+++ app.py
@@ -6,24 +6,21 @@ from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 st.title("ChatGPT-like clone")
 
 token_provider = get_bearer_token_provider(
-    DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
+    DefaultAzureCredential(), 'https://cognitiveservices.azure.com/.default'
 )
-
+print(os.getenv('AZURE_OPENAI_ENDPOINT'))
 client = AzureOpenAI(
-    azure_endpoint="https://cog-g4i4nkuqzsqug.openai.azure.com/",
+    azure_endpoint=os.getenv('AZURE_OPENAI_ENDPOINT'),
     azure_ad_token_provider=token_provider,
-    api_version="2024-06-01"
+    api_version='2024-06-01'
 )
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4o"
-
-if "messages" not in st.session_state:
+if 'messages' not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
 
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -32,7 +29,7 @@ if prompt := st.chat_input("What is up?"):
 
     with st.chat_message("assistant"):
         stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
+            model='gpt-4o',
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
